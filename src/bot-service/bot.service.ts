38e0d59f777e9da3293f413f343f22a2bbea7b02
Jakub <jmwierzbicki@gmail.com>
2022-10-07 16:49:03 +0200

    test environment

--- src/bot-service/bot.service.ts
+++ src/bot-service/bot.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class BotService {
   constructor() {
-    console.log('test!');
+    console.log('test!!');
     console.log(process.env.TOKEN);
   }
 }
--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { Sequelize } from 'sequelize';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CyclicDB = require('cyclic-dynamodb');
+const db = CyclicDB('zany-jade-toad-beltCyclicDB');
+
 
 @Injectable()
 export class DatabaseService {
   public sequelize;
 
   constructor() {
-    this.sequelize = new Sequelize({
-      dialect: 'sqlite',
-      storage: '.data/database.sqlite',
-    });
+    const run = async function () {
+      const animals = db.collection('animals');
+      const leo = await animals.set('leo', {
+        type: 'cat',
+        color: 'orange',
+      });
+    };
+    run();
   }
 }
