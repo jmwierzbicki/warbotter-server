9eea94ae7843369b44112730891d4db7df4b1cd6
Jakub <jmwierzbicki@gmail.com>
2022-10-07 17:15:56 +0200

    test usage of database in controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { DatabaseService } from './database/database.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private dbService: DatabaseService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    const animals = this.dbService.db.collection('animals');
+    const leo = await animals.set('leo', {
+      type: 'cat',
+      color: 'orange',
+    });
+    console.log(leo)
+    console.log(process.env);
+
+    const item = await animals.get('leo');
+    console.log(item);
+
+    return item;
   }
 }
--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,20 +1,15 @@
 import { Injectable } from '@nestjs/common';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const CyclicDB = require('cyclic-dynamodb');
-const db = CyclicDB('zany-jade-toad-beltCyclicDB');
 
 
 @Injectable()
 export class DatabaseService {
-  public sequelize;
+  public db = CyclicDB('zany-jade-toad-beltCyclicDB');
 
   constructor() {
     const run = async function () {
-      const animals = db.collection('animals');
-      const leo = await animals.set('leo', {
-        type: 'cat',
-        color: 'orange',
-      });
+
     };
     run();
   }
