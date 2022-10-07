7bf956214236cd17f073e567490863e487fc595a
Jakub <jmwierzbicki@gmail.com>
2022-10-08 01:17:54 +0200

    test typescript-sequelize

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Sequelize } from 'sequelize';
 @Injectable()
 export class DatabaseService {
-  public ds;
+  public db: Sequelize;
 
   constructor() {
-
+    this.db = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASS,
+      {
+        dialect: 'mysql',
+        host: process.env.DB_HOST,
+      },
+    );
+    this.db.authenticate().then(() => {
+      console.log('Connection has been established successfully.');
+    });
   }
 }
